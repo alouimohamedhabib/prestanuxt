0859f5b3614eb81d6b4991a67d5c72a90eb56016
Mohamed Habib ALOUI <mohamedhabibaloui@gmail.com>
2023-06-27 11:56:58 +0200

    feat: forward cookies differently

--- src/store/Page.ts
+++ src/store/Page.ts
@@ -20,7 +20,13 @@ export const usePageStore = defineStore('page', {
         async bootstrapApp() {
             const { data } = await useFetch("/api/bootstrap");
             console.log(data.value?.all);
-            
+            useCookie(
+                "POC",
+                {
+                    default: () => ("def5020067b7c82d0c6e09205a9f17302c68e92e5dc9c8a44c8d0636c416327019ec321caa0b33b89253b87a120b833314d3038ed7536ef8d4367e6a3e59233e3db395e2d6882e4ee5f15eb67044430aa1f566de8acdb6387c773f6970823e6792f5725baca08f3ca9ef6fbfdccf27d92408f5b27d6b7a574853e81d89976ab7b7bd40674ce77264408d65fed1ee2828cbf8bfeee76723ce61"),
+                    watch: false
+                }
+            )
             Object.keys(data.value?.all || {}).forEach(item => {
                 if (item.indexOf("PrestaShop") != -1) {
                     useCookie(
